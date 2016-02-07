f50194fd7c9625eec6eaae2e9e090b3152569fe6
ak-org <ashish@ashishkumar.org>
2016-02-07 15:59:46 -0800

    V0.6.1 - Display Event details in upcoming events and received events.

--- client/views/recvd/recvd.js
+++ client/views/recvd/recvd.js
@@ -1,12 +1,18 @@
 var recvdEvents;
+var indexer = 0;
 
 Meteor.subscribe('events', function() {
   console.log("Number of Recvd Events = "  + Events.find({"eventDetails.invitees.name" : Meteor.user().username}).count() );
 });
 
+Meteor.subscribe('profiles', function() {
+  console.log("Number of Events = "  + Profiles.find({}).count() );
+});
+
 
 Template.recvd.onRendered(function() {
         console.log(Meteor.user().username);
+        indexer = 0;
         
     }
 );
@@ -16,12 +22,63 @@ Template.recvd.onRendered(function() {
 
 	  // This code only runs on the client
   Template.recvd.helpers({
-    invitees: function() {
+        invitees: function() {
         
         recvdEvents = Events.find({"eventDetails.invitees.name" : Meteor.user().username});
             //console.log(userEvents.collection);
+            console.log(recvdEvents);
             return  recvdEvents;
         },
+
+        organizerFullName : function(username) {
+          fullName = Profiles.find({username: username}).fetch();
+          //console.log("find full name ", fullName[0].realname, username);
+          return fullName[0].realname;
+        },
+
+        listRestaurants : function (tracker, index, eventDetails) {
+          var response = "";
+          //console.log(selectedRestaurants);
+          //console.log("index is " + index.length, tracker);
+          //console.log(eventDetails.returnedResults[tracker]);
+       
+            if (index[tracker] === true) {
+               response = "<li class=\"list-group-item\">";
+               //response = eventDetails.returnedResults[index].name;
+               response += "<h4>" + eventDetails.returnedResults[tracker].name + "</h4>";
+               
+               if (eventDetails.returnedResults[tracker].rating) {
+                  response += "<h4> Rating " + eventDetails.returnedResults[tracker].rating + "/5, ";
+               }
+               
+               switch (eventDetails.returnedResults[tracker].price_level) {
+                case 1: 
+                   response += " $ </h4>";
+                   break;
+                case 2 :
+                   response += " $$ </h4>";
+                   break;
+                case 3 :
+                   response += " $$$ </h4>";
+                   break;
+                case 4 :
+                   response += " $$$$ </h4>";
+                   break;
+
+               }
+               response += "<h5>" + eventDetails.returnedResults[tracker].formatted_address + "</h5>";
+               response += "</li>";
+               console.log("Found a match", response);
+               return Spacebars.SafeString(response);
+            }
+          
+          
+        },
+
+        trackIndex : function() {
+            indexer++;
+            return indexer - 1;
+        }
       
   });
 
--- client/views/upcoming/upcoming.js
+++ client/views/upcoming/upcoming.js
@@ -5,6 +5,10 @@ Meteor.subscribe('events', function() {
   console.log("Number of Events = "  + Events.find({}).count() );
 });
 
+Meteor.subscribe('profiles', function() {
+  console.log("Number of Events = "  + Profiles.find({}).count() );
+});
+
 
 Template.upcoming.onRendered(function() {
         console.log(Meteor.user().username);
@@ -22,7 +26,24 @@ Template.upcoming.helpers({
         },
     totalEvents : function () {
            return Events.find({}).count() ;
-    }        
+    },
+
+    organizerFullName : function(username) {
+        fullName = Profiles.find({username: username}).fetch();
+        //console.log("find full name ", fullName[0].realname, username);
+        return fullName[0].realname;
+    },
+
+    eventStatus : function (invitees) {
+         var eventStatus = "Confirmed";
+         //console.log (invitees);
+         for (var i = 0; i < invitees.length; i++) {
+            if (invitees.response !== "Accepted") {
+              eventStatus = "Pending Confirmation";
+            }
+         }
+         return eventStatus;
+    }       
     
   });
 
